91ddc5880aa8a8b72da20b6e25b7d0eb6be02e21
bandai2002sl <bandai2002sl@gmail.com>
2023-09-28 18:44:50 +0700

    update đăng xuất

--- src/components/layout/BaseLayout/components/Header/Header.tsx
+++ src/components/layout/BaseLayout/components/Header/Header.tsx
@@ -1,90 +1,95 @@
-// Trong header.tsx
 
 import { PropsHeader } from "./interfaces";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./Header.module.scss";
-import React, { useState } from "react";
-import { Icon } from "iconsax-react";
+
 
 function Header({}: PropsHeader) {
-  const [showProvinces, setShowProvinces] = useState(false);
-  const [showDistricts, setShowDistricts] = useState(false);
-  const [showCommunities, setShowCommunities] = useState(false);
+  //==========================Phan 5-============================================================
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
 
-  const toggleProvinces = () => setShowProvinces(!showProvinces);
-  const toggleDistricts = () => setShowDistricts(!showDistricts);
-  const toggleCommunities = () => setShowCommunities(!showCommunities);
+  const toggleSettings = () => {
+    setIsSettingsOpen(!isSettingsOpen);
+  };
+ //============================================================================================
+
+ //==============================Phan5-Dang xuat ==========================================================
+ const handleLogout = async () => {
+  try {
+    // Gửi yêu cầu đến máy chủ hoặc API để đăng xuất
+    const response = await fetch('/api/logout', {
+      method: 'POST', // Hoặc 'GET', 'DELETE', tùy vào cách bạn đã thiết lập API
+      credentials: 'include', 
+    });
+
+    if (response.ok) {
+      // Đăng xuất thành công
+      window.location.reload(); // Tải lại trang để đăng nhập lại hoặc chuyển hướng đến trang đăng nhập
+    } else {
+      // Xử lý lỗi đăng xuất
+      console.error('Không thể đăng xuất');
+    }
+  } catch (error) {
+    // Xử lý lỗi gửi yêu cầu
+    console.error('Lỗi khi gửi yêu cầu đăng xuất', error);
+  }
+};
+ //============================================================================================
 
   return (
     <div className={styles.container}>
-      {/* Phần 1 */}
-      <div className={styles.section}>
-        <div className={styles.icon}>
 
+        {/* Phần 1 */}
+        <div className={styles.section1}>
+         <img
+            className={styles.logo}
+            src="/images/icon-logo.png"
+            alt="Logo trang web"
+          />
         </div>
-      </div>
-
-      {/* Phần 2 */}
-        <div className={styles.section}>
-          <div className={`${styles.title} ${styles.bold}`}>
+      
+        {/* Phần 2 */}
+        <div className={styles.section2}>
+          <div className={styles.title}>
             QUẢN LÝ NÔNG NGHIỆP NINH BÌNH
           </div>
-      </div>
-
-      {/* Phần 3 */}
-      <div className={styles.section}>
-        <div
-          className={`${styles.locationBox} ${
-            showProvinces ? styles.active : ""
-          }`}
-          onClick={toggleProvinces}
-        >
-          Tỉnh▼
-          {showProvinces && (
+        </div>
+      
+        {/* Phần 3 */}
+        <div className={styles.section3}>
+          <div className={styles.locationBox}>
+            <div className={styles.locationTitle}>Tỉnh</div>
             <ul className={styles.locationList}>
               {/* Danh sách các tỉnh */}
               <li>Tỉnh 1</li>
               <li>Tỉnh 2</li>
-              
-            </ul>
-          )}
-        </div>
-
-        <div
-          className={`${styles.locationBox} ${
-            showDistricts ? styles.active : ""
-          }`}
-          onClick={toggleDistricts}
-        >
-          Huyện▼
-          {showDistricts && (
-            <ul className={styles.locationList}>
+              </ul>
+           </div>
+  
+        <div className={styles.locationBox}>
+          <div className={styles.locationTitle}>Huyện</div>
+          <ul className={styles.locationList}>
               {/* Danh sách các huyện */}
               <li>Huyện 1</li>
               <li>Huyện 2</li>
             </ul>
-          )}
         </div>
-
-        <div
-          className={`${styles.locationBox} ${
-            showCommunities ? styles.active : ""
-          }`}
-          onClick={toggleCommunities}
-        >
-          Xã▼
-          {showCommunities && (
-            <ul className={styles.locationList}>
+  
+        <div className={styles.locationBox}>
+          <div className={styles.locationTitle}>Xã</div>
+          <ul className={styles.locationList}>
               {/* Danh sách các xã */}
               <li>Xã 1</li>
               <li>Xã 2</li>
             </ul>
-          )}
         </div>
       </div>
 
+
+
       {/* Phần 4 */}
-      <div className={styles.section}>
-        <div className={styles.search}>
+      <div className={styles.section4}>
+      <div className={styles.search}>
           <div className={styles.searchIcon}>
 
           </div>
@@ -95,7 +100,26 @@ function Header({}: PropsHeader) {
           />
         </div>
       </div>
+    
+    {/* Phần 5 */}
+    <div className={styles.section5}>
+        <div className={styles.settingsBox}>
+          <img
+            className={styles.settingsIcon}
+            src="/images/settings-icon.png"
+            alt="Cài đặt"
+            onClick={toggleSettings}
+          />
+          <ul className={styles.settingsList}>
+            <li>
+            <button className={styles.logout_button} onClick={handleLogout}>Đăng xuất</button>
+            </li>
+          
+          </ul>
+        </div>
+      </div>
     </div>
+    
   );
 }
 

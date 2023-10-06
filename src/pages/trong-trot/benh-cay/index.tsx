d476d224b0015f0806f6bd80194d17912a267123
bandai2002sl <bandai2002sl@gmail.com>
2023-10-06 18:03:22 +0700

    update bảng phần benh-cay

--- src/pages/trong-trot/benh-cay/index.tsx
+++ src/pages/trong-trot/benh-cay/index.tsx
@@ -1,62 +1,78 @@
-import { Fragment, ReactElement } from "react";
-import BaseLayout from "~/components/layout/BaseLayout";
-import Head from "next/head";
-import i18n from "~/locale/i18n";
-import styles from "~/pages/khung.module.css";
-import tableStyles from "~/pages/khung.module.css";
+// Import các module cần thiết
+import { Fragment, ReactElement, useState, useEffect } from 'react';
+import BaseLayout from '~/components/layout/BaseLayout';
+import Head from 'next/head';
+import i18n from '~/locale/i18n';
+import styles from '~/pages/khung.module.css';
+import tableStyles from '~/pages/khung.module.css';
+import benhCayServices from '~/services/benhCaySevices'; 
 
+interface UserData {
+  id: number;
+  CayTrongId: number;
+  LoaiBenhId: number;
+  DiaChi: string;
+  MoTa: string;
+  HinhAnh: string;
+  NgayGhiNhan: string;
+}
+
+// Function để hiển thị nội dung trang
 export default function Page() {
-  const userData = [
-    // Dữ liệu mẫu người dùng
-    { id: 1, CayTrongId: "1", LoaiBenhId:23,  DonViHanhChinhId: "HN" , DiaChi:"Gia Lâm, HN",MoTa:"Bị sâu",HinhAnh:"anh.jpg", DienTich:"12Ha",  NgayGhiNhan: "23/4/2023",CreatedById: "DaiVu",LasUserId: "daivu10",LastTime:"4:25,10/6/2023", CreatedOn:"5:25,1/2/2023"},
-    { id: 1, CayTrongId: "1", LoaiBenhId:23,  DonViHanhChinhId: "HN" , DiaChi:"Gia Lâm, HN",MoTa:"Bị sâu",HinhAnh:"anh.jpg", DienTich:"12Ha",  NgayGhiNhan: "23/4/2023",CreatedById: "DaiVu",LasUserId: "daivu10",LastTime:"4:25,10/6/2023", CreatedOn:"5:25,1/2/2023"},
-    { id: 1, CayTrongId: "1", LoaiBenhId:23,  DonViHanhChinhId: "HN" , DiaChi:"Gia Lâm, HN",MoTa:"Bị sâu",HinhAnh:"anh.jpg", DienTich:"12Ha",  NgayGhiNhan: "23/4/2023",CreatedById: "DaiVu",LasUserId: "daivu10",LastTime:"4:25,10/6/2023", CreatedOn:"5:25,1/2/2023"},
-    { id: 1, CayTrongId: "1", LoaiBenhId:23,  DonViHanhChinhId: "HN" , DiaChi:"Gia Lâm, HN",MoTa:"Bị sâu",HinhAnh:"anh.jpg", DienTich:"12Ha",  NgayGhiNhan: "23/4/2023",CreatedById: "DaiVu",LasUserId: "daivu10",LastTime:"4:25,10/6/2023", CreatedOn:"5:25,1/2/2023"},
-    { id: 1, CayTrongId: "1", LoaiBenhId:23,  DonViHanhChinhId: "HN" , DiaChi:"Gia Lâm, HN",MoTa:"Bị sâu",HinhAnh:"anh.jpg", DienTich:"12Ha",  NgayGhiNhan: "23/4/2023",CreatedById: "DaiVu",LasUserId: "daivu10",LastTime:"4:25,10/6/2023", CreatedOn:"5:25,1/2/2023"},
-  ];
+  const [userData, setUserData] = useState<UserData[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await benhCayServices.fetchDataFromDatabase({
+          "cropTypeId": 0,
+          "loaiBenhId": 0,
+          "administrativeUnitId": 0,
+          "diaChi": "string",
+          "moTa": "string",
+          "hinhAnh": "string",
+          "dienTich": 0,
+          "ngayGhiNhan": "string"
+        });
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Lỗi khi lấy dữ liệu người dùng:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Fragment>
       <Head>
-        <title>{i18n.t("Trồng trọt")}</title>
+        <title>{i18n.t('Trồng trọt')}</title>
       </Head>
-      
+
       <div className={styles.contentContainer}>
-    <h2 className={styles.title}>Bệnh của cây</h2>
-    <table className={`${styles.table} ${tableStyles.tableWithBorder}`}>
+        <h2 className={styles.title}>Bệnh của cây</h2>
+        <table className={`${styles.table} ${tableStyles.tableWithBorder}`}>
           <thead>
             <tr>
               <th>ID</th>
               <th>ID Cây trồng</th>
               <th>ID Loại bệnh</th>
-              <th>ID Đơn vị hành chính</th>
               <th>Địa chỉ</th>
               <th>Mô tả</th>
               <th>Hình ảnh</th>
-              <th>Diện tích</th>
               <th>Ngày ghi nhận</th>
-              <th>ID người tạo</th>
-              <th>ID Người cuối cùng</th>
-              <th>Cập nhật lần cuối</th>
-              <th>Được tạo vào ngày</th>
             </tr>
           </thead>
           <tbody>
-            {userData.map((user) => (
+            {userData && userData.map((user) => (
               <tr key={user.id}>
                 <td>{user.id}</td>
                 <td>{user.CayTrongId}</td>
                 <td>{user.LoaiBenhId}</td>
-                <td>{user.DonViHanhChinhId}</td>
                 <td>{user.DiaChi}</td>
                 <td>{user.MoTa}</td>
                 <td>{user.HinhAnh}</td>
-                <td>{user.DienTich}</td>
-                <td>{user.NgayGhiNhan}</td>     
-                <td>{user.CreatedById}</td>  
-                <td>{user.LasUserId}</td>  
-                <td>{user.LastTime}</td>  
-                <td>{user.CreatedOn}</td>            
+                <td>{user.NgayGhiNhan}</td>
               </tr>
             ))}
           </tbody>
@@ -66,6 +82,7 @@ export default function Page() {
   );
 }
 
+// Function để chỉ định bố cục cho trang
 Page.getLayout = function (page: ReactElement) {
   return <BaseLayout>{page}</BaseLayout>;
 };

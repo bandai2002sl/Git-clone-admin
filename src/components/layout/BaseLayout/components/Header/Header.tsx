ad43e047f5ee7bc18e1c310834ecb55ad4deeaa1
Nguyen Duc Duy <140948540+DevNgDuy@users.noreply.github.com>
2023-10-07 18:56:56 +0700

    Update Header.tsx

--- src/components/layout/BaseLayout/components/Header/Header.tsx
+++ src/components/layout/BaseLayout/components/Header/Header.tsx
@@ -1,45 +1,107 @@
-
 import { PropsHeader } from "./interfaces";
 import React, { useState, useRef, useEffect } from "react";
 import styles from "./Header.module.scss";
 
 
+
 function Header({}: PropsHeader) {
-  //==========================Phan 5-============================================================
-  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  //=================================================================================================
+  const [isProvinceListVisible, setIsProvinceListVisible] = useState(false);
+  const [isDistrictListVisible, setIsDistrictListVisible] = useState(false);
+  const [isCommuneListVisible, setIsCommuneListVisible] = useState(false);
 
-  const toggleSettings = () => {
-    setIsSettingsOpen(!isSettingsOpen);
+  const provinceBoxRef = useRef<HTMLDivElement>(null);
+  const districtBoxRef = useRef<HTMLDivElement>(null);
+  const communeBoxRef = useRef<HTMLDivElement>(null);
+
+
+  const toggleProvinceList = () => {
+    setIsProvinceListVisible(!isProvinceListVisible);
+    setIsDistrictListVisible(false);
+    setIsCommuneListVisible(false);
   };
- //============================================================================================
+  const toggleDistrictList = () => {
+    setIsProvinceListVisible(false);
+    setIsDistrictListVisible(!isDistrictListVisible);
+    setIsCommuneListVisible(false);
+  };
+  const toggleCommuneList = () => {
+    setIsProvinceListVisible(false);
+    setIsDistrictListVisible(false);
+    setIsCommuneListVisible(!isCommuneListVisible);
+  };
 
- //==============================Phan5-Dang xuat ==========================================================
- const handleLogout = async () => {
-  try {
-    // Gửi yêu cầu đến máy chủ hoặc API để đăng xuất
-    const response = await fetch('/api/logout', {
-      method: 'POST', // Hoặc 'GET', 'DELETE', tùy vào cách bạn đã thiết lập API
-      credentials: 'include', 
-    });
 
-    if (response.ok) {
-      // Đăng xuất thành công
-      window.location.reload(); // Tải lại trang để đăng nhập lại hoặc chuyển hướng đến trang đăng nhập
-    } else {
-      // Xử lý lỗi đăng xuất
-      console.error('Không thể đăng xuất');
-    }
-  } catch (error) {
-    // Xử lý lỗi gửi yêu cầu
-    console.error('Lỗi khi gửi yêu cầu đăng xuất', error);
-  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        provinceBoxRef.current &&
+        !provinceBoxRef.current.contains(event.target as Node)
+      ) {
+        setIsProvinceListVisible(false);
+      }
+
+      if (
+        districtBoxRef.current &&
+        !districtBoxRef.current.contains(event.target as Node)
+      ) {
+        setIsDistrictListVisible(false);
+      }
+
+      if (
+        communeBoxRef.current &&
+        !communeBoxRef.current.contains(event.target as Node)
+      ) {
+        setIsCommuneListVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [provinceBoxRef, districtBoxRef, communeBoxRef]);
+ //=================================================================================================
+ const provinces: string[] = ["Ninh Bình"];
+const districtsByProvince: Record<string, string[]> = {
+  "Ninh Bình": ["Huyện 1", "Huyện 2", "Huyện 3"],
 };
- //============================================================================================
+const communesByDistrict: Record<string, string[]> = {
+  "Huyện 1": ["Xã 1", "Xã 2", "Xã 3"],
+  "Huyện 2": ["Xã 4", "Xã 5", "Xã 6"],
+  "Huyện 3": ["Xã 7", "Xã 8", "Xã 9"],
+};
+
+  const [selectedProvince, setSelectedProvince] = useState<string | null>(null);
+  const [selectedDistrict, setSelectedDistrict] = useState<string | null>(null);
+  const [selectedCommune, setSelectedCommune] = useState<string | null>(null);
+
+  const handleProvinceSelect = (province: string) => {
+    setSelectedProvince(province);
+    setSelectedDistrict(null);
+    setSelectedCommune(null);
+    setIsProvinceListVisible(false);
+  };
+
+  const handleDistrictSelect = (district: string) => {
+    setSelectedDistrict(district);
+    setSelectedCommune(null);
+    setIsDistrictListVisible(false);
+  };
+
+  const handleCommuneSelect = (commune: string) => {
+    setSelectedCommune(commune);
+    setIsCommuneListVisible(false);
+  };
+
+ //=================================================================================================
 
   return (
     <div className={styles.container}>
 
-        {/* Phần 1 */}
+        
         <div className={styles.section1}>
          <img
             className={styles.logo}
@@ -48,50 +110,73 @@ function Header({}: PropsHeader) {
           />
         </div>
       
-        {/* Phần 2 */}
+        
         <div className={styles.section2}>
           <div className={styles.title}>
             QUẢN LÝ NÔNG NGHIỆP NINH BÌNH
           </div>
+          
         </div>
       
-        {/* Phần 3 */}
+        
         <div className={styles.section3}>
-          <div className={styles.locationBox}>
-            <div className={styles.locationTitle}>Tỉnh</div>
+
+        <div
+          className={styles.locationBox}
+          onClick={toggleProvinceList}
+          ref={provinceBoxRef}
+        >
+          <div className={styles.locationTitle}>{selectedProvince || "Tỉnh"}</div>
+          {isProvinceListVisible && (
             <ul className={styles.locationList}>
-              {/* Danh sách các tỉnh */}
-              <li>Tỉnh 1</li>
-              <li>Tỉnh 2</li>
-              </ul>
-           </div>
-  
-        <div className={styles.locationBox}>
-          <div className={styles.locationTitle}>Huyện</div>
-          <ul className={styles.locationList}>
-              {/* Danh sách các huyện */}
-              <li>Huyện 1</li>
-              <li>Huyện 2</li>
+              {provinces.map((province) => (
+                <li key={province} onClick={() => handleProvinceSelect(province)}>
+                  {province}
+                </li>
+              ))}
             </ul>
+          )}
         </div>
-  
-        <div className={styles.locationBox}>
-          <div className={styles.locationTitle}>Xã</div>
-          <ul className={styles.locationList}>
-              {/* Danh sách các xã */}
-              <li>Xã 1</li>
-              <li>Xã 2</li>
+
+        <div
+          className={styles.locationBox}
+          onClick={toggleDistrictList}
+          ref={districtBoxRef}
+        >
+          <div className={styles.locationTitle}>{selectedDistrict || "Huyện"}</div>
+          {isDistrictListVisible && selectedProvince && (
+            <ul className={styles.locationList}>
+              {districtsByProvince[selectedProvince].map((district) => (
+                <li key={district} onClick={() => handleDistrictSelect(district)}>
+                  {district}
+                </li>
+              ))}
             </ul>
+          )}
+        </div>
+
+        <div
+          className={styles.locationBox}
+          onClick={toggleCommuneList}
+          ref={communeBoxRef}
+        >
+          <div className={styles.locationTitle}>{selectedCommune || "Xã"}</div>
+          {isCommuneListVisible && selectedDistrict && (
+            <ul className={styles.locationList}>
+              {communesByDistrict[selectedDistrict].map((commune) => (
+                <li key={commune} onClick={() => handleCommuneSelect(commune)}>
+                  {commune}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
 
-
-
-      {/* Phần 4 */}
-      <div className={styles.section4}>
-      <div className={styles.search}>
+        
+        <div className={styles.section4}>
+        <div className={styles.search}>
           <div className={styles.searchIcon}>
-
           </div>
           <input
             type="text"
@@ -99,28 +184,16 @@ function Header({}: PropsHeader) {
             className={styles.input}
           />
         </div>
-      </div>
-    
-    {/* Phần 5 */}
-    <div className={styles.section5}>
-        <div className={styles.settingsBox}>
-          <img
-            className={styles.settingsIcon}
-            src="/images/settings-icon.png"
-            alt="Cài đặt"
-            onClick={toggleSettings}
-          />
-          <ul className={styles.settingsList}>
-            <li>
-            <button className={styles.logout_button} onClick={handleLogout}>Đăng xuất</button>
-            </li>
-          
-          </ul>
         </div>
-      </div>
+    
+       
+        <div className={styles.section5}>
+          <button>Đăng xuất</button>
+        </div>
     </div>
     
   );
-}
+  
 
+}
 export default Header;

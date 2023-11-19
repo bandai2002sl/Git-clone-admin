b28cb528beecb271153ad4b6481479234c02ad14
bandai2002sl <bandai2002sl@gmail.com>
2023-11-20 01:27:53 +0700

    Your commit message

--- src/pages/pt-thuy-loi/cong/index.tsx
+++ src/pages/pt-thuy-loi/cong/index.tsx
@@ -1,39 +1,48 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { ReactElement } from 'react';
-import BaseLayout from '~/components/layout/BaseLayout';
-import Head from 'next/head';
-import i18n from '~/locale/i18n';
-import styles from '../../manage.module.scss';
-import AddNewItemModal from '../../../components/page/thuy-san/quan-ly-tau-ca/modalAddNew';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import ModalEdit from '../../../components/page/thuy-san/quan-ly-tau-ca/modalEdit';
-import congServices from "~/services/congServices";
-import Button from "~/components/common/Button";
+import React, { useState, useEffect } from "react";
+import BaseLayout from "~/components/layout/BaseLayout";
+import Head from "next/head";
+import i18n from "~/locale/i18n";
+import styles from "../../manage.module.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import tramBomServices from "~/services/tramBomServices";
+import EditItemModal from "~/components/page/thuy-loi/tram-bom/EditItemModal";
+import AddItemModal from "~/components/page/thuy-loi/tram-bom/AddItemModal";
 
-export default function Page() {
-  const [data, setData] = useState<any>([]);
+interface DataItem {
+  id: number;
+  ten: string;
+  diaChi: string;
+  congXuat: number;
+  loaiHinh: string;
+  administrativeUnitId: number;
+}
 
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const [newItem, setNewItem] = useState<any>({
-    ten: 'string',
-    diaChi: 'string',
-    kichCo: 'string',
-    loaiKichThuoc: 'string',
-    loaiHinh: 'string',
+function YourComponent() {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [selectedItem, setSelectedItem] = useState<DataItem | null>(null);
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+
+  const [newItem, setNewItem] = useState<DataItem>({
+    id: 0,
+    ten: "",
+    diaChi: "",
+    congXuat: 0,
+    loaiHinh: "",
     administrativeUnitId: 0,
   });
-  const [editedData, setEditedData] = useState<any>({});
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
-  const [itemToDelete, setItemToDelete] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await congServices.displayCong(data);
-        const newData = response.data;
-        setData(newData);
+        const response = await tramBomServices.display({
+          ten: "",
+          diaChi: "",
+          congXuat: 0,
+          loaiHinh: "",
+          administrativeUnitId: 0,
+        });
+        setData(response.data);
       } catch (error) {
         console.error(error);
       }
@@ -41,146 +50,114 @@ export default function Page() {
     fetchData();
   }, []);
 
-  const handleAdd = async () => {
-    try {
-      const response = await congServices.createCong(newItem);
-      setData([...data, response.data]);
-      setIsAddModalOpen(false);
-      setNewItem({
-        ten: 'string',
-        diaChi: 'string',
-        kichCo: 'string',
-        loaiKichThuoc: 'string',
-        loaiHinh: 'string',
-        administrativeUnitId: 0,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  const handleEdit = (item: any) => {
-    setEditedData(item);
-    setIsEditModalOpen(true);
+  // Hàm mở modal chỉnh sửa
+  const openEditModal = (item: DataItem) => {
+    setSelectedItem(item);
+    setEditModalOpen(true);
   };
 
-  const handleUpdate = async (editedItem: any) => {
-    try {
-      const response = await congServices.updateCong(editedItem.id, editedItem);
-      const updatedData = data.map((item: any) =>
+  // Hàm chỉnh sửa mục
+  const handleEdit = (editedItem: DataItem) => {
+    // Gửi yêu cầu chỉnh sửa thông tin đến API
+    tramBomServices
+      .update(editedItem.id, editedItem)
+      .then(() => {
+        // Cập nhật danh sách dữ liệu
+        const updatedData = data.map((item) =>
           item.id === editedItem.id ? editedItem : item
-      );
-      setData(updatedData);
-      setIsEditModalOpen(false);
-      setEditedData(null);
-    } catch (error) {
-      console.error(error);
-    }
+        );
+        setData(updatedData);
+        setEditModalOpen(false);
+      })
+      .catch((error) => {
+        console.error("Chỉnh sửa không thành công:", error);
+      });
   };
 
-  const handleDelete = (deleteItem: any) => {
-    setItemToDelete(deleteItem);
-    setIsConfirmDeleteOpen(true);
+  // Hàm thêm mới mục
+  const handleAdd = (newItem: DataItem) => {
+    // Gửi yêu cầu thêm mới thông tin đến API
+    tramBomServices
+      .create(newItem)
+      .then((response) => {
+        const addedItem = response.data; // Lấy về mục đã được thêm mới từ API
+        setData([...data, addedItem]); // Thêm mục vào danh sách
+        setAddModalOpen(false);
+      })
+      .catch((error) => {
+        console.error("Thêm mới không thành công:", error);
+      });
   };
 
-  const handleConfirmDelete = async (deleteItem: any) => {
-    try {
-      const response = await congServices.deleteCong(deleteItem.id);
-      const updatedData = data.filter((dataItem: any) => dataItem.id !== deleteItem.id);
-      setData(updatedData);
-      setIsConfirmDeleteOpen(false);
-      setItemToDelete(null);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleCancelDelete = () => {
-    setIsConfirmDeleteOpen(false);
-    setItemToDelete(null);
+  // Hàm xóa mục
+  const handleDelete = (item: DataItem) => {
+    // Gửi yêu cầu xóa thông tin đến API
+    tramBomServices
+      .delete(item.id)
+      .then(() => {
+        // Xoá mục dữ liệu khỏi danh sách
+        const updatedData = data.filter((dataItem) => dataItem.id !== item.id);
+        setData(updatedData);
+      })
+      .catch((error) => {
+        console.error("Xoá không thành công:", error);
+      });
   };
 
   return (
-      <Fragment>
-        <Head>
-          <title>{i18n.t('Quản lý công')}</title>
-        </Head>
-        <div>
-          <Button primary bold rounded_4 maxContent onClick={() => setIsAddModalOpen(true)}>&#x002B; Thêm </Button>
-
-          {isAddModalOpen && (
-              <AddNewItemModal
-                  isOpen={isAddModalOpen}
-                  onClose={() => {
-                    setIsAddModalOpen(false);
-                  }}
-                  onSubmit={handleAdd}
-                  newItem={newItem}
-                  setNewItem={setNewItem}
-              />
-          )}
-        </div>
-        <table className={styles['customers']}>
+    <BaseLayout>
+      <Head>
+        <title>{i18n.t("Trạm bơm")}</title>
+      </Head>
+      <div>
+        <button onClick={() => setAddModalOpen(true)}>Thêm Mới</button>
+        <table className={styles["customers"]}>
           <thead>
-          <tr>
-            <th>Tên</th>
-            <th>Địa chỉ</th>
-            <th>Kích cỡ</th>
-            <th>Loại kích thước</th>
-            <th>Loại hình</th>
-            <th>Hoạt động</th>
-          </tr>
+            <tr>
+              <th>Tên</th>
+              <th>Địa chỉ</th>
+              <th>Công xuất</th>
+              <th>Loại hình</th>
+              <th>Đơn vị hành chính Id</th>
+              <th>Hoạt động</th>
+            </tr>
           </thead>
           <tbody>
-          {data.map((item: any) => (
+            {data.map((item: DataItem) => (
               <tr key={item.id}>
                 <td>{item.ten}</td>
                 <td>{item.diaChi}</td>
-                <td>{item.kichCo}</td>
-                <td>{item.loaiKichThuoc}</td>
+                <td>{item.congXuat}</td>
                 <td>{item.loaiHinh}</td>
+                <td>{item.administrativeUnitId}</td>
                 <td>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <Button primary bold rounded_4 maxContent onClick={() => handleEdit(item)}>&#9998; Sửa</Button>
-                    {isEditModalOpen && (
-                        <ModalEdit
-                            isOpen={isEditModalOpen}
-                            onClose={() => {
-                              setIsEditModalOpen(false);
-                            }}
-                            onUpdate={handleUpdate}
-                            editedItemId={editedData.id}
-                            setEditedData={setEditedData}
-                            editedData={editedData}
-                        />
-                    )}
-                    <div style={{ marginLeft: '10px' }}>
-                      <Button primary bold rounded_4 maxContent onClick={() => handleDelete(item)}>&#10060; </Button>
-                    </div>
-                  </div>
-                  {isConfirmDeleteOpen && (
-                      <Modal isOpen={isConfirmDeleteOpen} backdrop={false}>
-                        <ModalHeader>Xác nhận xóa</ModalHeader>
-                        <ModalBody>Bạn có chắc chắn muốn xóa?</ModalBody>
-                        <ModalFooter>
-                          <Button color="primary" onClick={() => handleConfirmDelete(itemToDelete)}>
-                            Có
-                          </Button>
-                          <Button color="secondary" onClick={handleCancelDelete}>
-                            Không
-                          </Button>
-                        </ModalFooter>
-                      </Modal>
-                  )}
+                  <button onClick={() => openEditModal(item)}>Sửa</button>
+                  <button onClick={() => handleDelete(item)}>Xoá</button>
                 </td>
               </tr>
-          ))}
+            ))}
           </tbody>
         </table>
-      </Fragment>
+      </div>
+
+      {/* Modal Thêm Mới */}
+      <AddItemModal
+        isOpen={isAddModalOpen}
+        onClose={() => setAddModalOpen(false)}
+        onSubmit={handleAdd}
+        newItem={newItem}
+        setNewItem={setNewItem}
+      />
+
+      {/* Modal Chỉnh Sửa */}
+      <EditItemModal
+        isOpen={isEditModalOpen}
+        onClose={() => setEditModalOpen(false)}
+        item={selectedItem}
+        onEdit={handleEdit}
+      />
+    </BaseLayout>
   );
 }
 
-Page.getLayout = function (page: ReactElement) {
-  return <BaseLayout>{page}</BaseLayout>;
-};
+export default YourComponent;
